000225b7f5a5627364b188a0c7b52193b017de9a
AceTBan <esteban_david@ymail.com>
2025-02-11 19:00:26 +0100

    ajout heure mercure

--- heure mars.py
+++ heure mars.py
@@ -3,39 +3,42 @@ import requests
 import time
 
 def obtenir_heure_mars():
-    # Définir l'URL de l'API qui fournit les données de Mars
     URL = "https://api.le-systeme-solaire.net/rest/bodies/mars"
-
-    # Faire une requête GET à l'API et récupérer le résultat au format JSON
     response = requests.get(URL)
     data = response.json()
-
-    # Extraire le paramètre sideralRotation qui correspond à l'heure sidérale de Mars en heures
     sideral_rotation = data["sideralRotation"]
-
-    # Calculer l'heure martienne 
-    mars_hours_per_day = 24.6229  # Durée d'un jour martien en heures terrestres
-
-    # Obtenir l'heure actuelle sur Terre
+    mars_hours_per_day = 24.6229  
     current_time = time.time()
     current_earth_time = datetime.datetime.now().time()
-
-    # Conversion du temps actuel terrestre en temps martien
     mars_time_in_earth_seconds = (current_time % (mars_hours_per_day * 3600))
     mars_hours = int((mars_time_in_earth_seconds // 3600) % 24)
     mars_minutes = int((mars_time_in_earth_seconds % 3600) // 60)
     mars_seconds = int(mars_time_in_earth_seconds % 60)
-
-    # Créer un objet datetime avec l'heure de mars
     mars_time = datetime.time(mars_hours, mars_minutes, mars_seconds)
-
     return current_earth_time, mars_time
+
+def obtenir_heure_mercure():
+    URL = "https://api.le-systeme-solaire.net/rest/bodies/mercury"
+    response = requests.get(URL)
+    data = response.json()
+    sideral_rotation = data["sideralRotation"]
+    mercury_hours_per_day = 1407.5  # Durée d'un jour mercurien en heures terrestres
+    current_time = time.time()
+    current_earth_time = datetime.datetime.now().time()
+    mercury_time_in_earth_seconds = (current_time % (mercury_hours_per_day * 3600))
+    mercury_hours = int((mercury_time_in_earth_seconds // 3600) % 24)
+    mercury_minutes = int((mercury_time_in_earth_seconds % 3600) // 60)
+    mercury_seconds = int(mercury_time_in_earth_seconds % 60)
+    mercury_time = datetime.time(mercury_hours, mercury_minutes, mercury_seconds)
+    return mercury_time
 
 while True:
     current_earth_time, mars_time = obtenir_heure_mars()
+    mercury_time = obtenir_heure_mercure()
 
     print(f"L'heure actuelle sur Terre est : {current_earth_time}")
     print(f"L'heure actuelle sur Mars est : {mars_time}")
+    print(f"L'heure actuelle sur Mercure est : {mercury_time}")
 
     choix = input("Appuyez sur 'r' pour rafraîchir, 'q' pour quitter : ")
     if choix.lower() == 'q':
